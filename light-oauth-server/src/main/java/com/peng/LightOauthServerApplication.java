a7d6c80680ec39ace61fc1340579acef2a16b474
pengchengqin@jomoo.cn <pengchengqin@jomoo.cn>
2019-12-26 12:57:55 +0800

    初始化工程

---
+++ light-oauth-server/src/main/java/com/peng/LightOauthServerApplication.java
@@ -0,0 +1,13 @@
+package com.peng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightOauthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightOauthServerApplication.class, args);
+    }
+
+}
